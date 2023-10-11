3908afcb754905c6d6f4a0eb05e95cb05a14b5da
Amir Ravan Nakhjavani <amir.h.rnn@gmail.com>
2023-10-11 20:50:24 +0330

    add output for results

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 
 
 class KotlinInterface:
-    FUNCTION_REGEX = 'fun\\s*([\\w<>]*)\\s*[(]'
+    FUNCTION_REGEX = 'fun\\s*([\\w<>]*)\\s*[(][@\\w\\d\\s]*:*([\\w\\d\\s]*)[)]'
     ATTRIBUTE_REGEX = '((val)|(var))\\s*(\\w*)'
 
     def __init__(self, name):
@@ -25,9 +25,9 @@ class KotlinInterface:
         return f"{self.name} : {self.functions} {self.attributes}"
 
     def collect(self, line):
-        search_result = re.search(self.FUNCTION_REGEX,line)
+        search_result = re.search(self.FUNCTION_REGEX, line)
         if search_result is not None:
-            self._add_function(search_result.group(1))
+            self._add_function(search_result.group(0))
         else:
             search_result = re.search(self.ATTRIBUTE_REGEX, line)
             if search_result is not None:
@@ -37,20 +37,28 @@ class KotlinInterface:
 
 
 class KotlinClass:
-    OVER_RIDE_REGEX = 'override\\s+((val)|(var))\\s+(\\w+)'
-    INPUT_REGEX = '(\\w*)\\s*:\\s*(\\w*(<\\w*\\?*>)?)'
+    OVER_RIDE_REGEX = 'override\\s+(\\w*\\s*)?((val)|(var)|(fun))\\s+(\\w+)'
+    INPUT_REGEX = '(\\w+)\\s*:\\s*(\\w*(<\\w*\\?*>)?)'
 
-    def __init__(self, name):
+    def __init__(self, name, class_type):
         self.name = name
+        self.class_type = class_type
         self.inputs = []
         self.overrides = []
         self.shouldContinue = True
+        self.checkForInput = True
 
     def _add_input(self, attribute):
         self.inputs.append(attribute)
 
     def _add_override(self, function):
         self.overrides.append(function)
+
+    def get_signature(self):
+        if self.class_type == "":
+            return "class " + self.name + " {"
+        else:
+            return self.class_type + " class " + self.name + " {"
 
     def __str__(self):
         return f"{self.name} : {self.inputs} {self.overrides}"
@@ -61,17 +69,19 @@ class KotlinClass:
     def collect(self, line):
         search_result = re.search(self.OVER_RIDE_REGEX, line)
         if search_result is not None:
-            self._add_override(search_result.group(4))
-        else:
+            self._add_override(search_result.group(6))
+        elif self.checkForInput:
             search_result = re.search(self.INPUT_REGEX, line)
-            if search_result is not None:
+            if search_result is not None and search_result.group(2) not in EXCLUDE_LIST:
                 self._add_input(search_result.group(2))
         if "{" in line:
-            self.shouldContinue = False
+            self.checkForInput = False
 
 
 interface_list = []
 class_list = []
+EXCLUDE_LIST = ["CoroutineDispatcherProvider"]
+OUTPUT_FILE_PATH = "./out.txt"
 
 
 def print_files(path):
@@ -87,29 +97,55 @@ def print_files(path):
 
 
 def check_file(file_path):
-    CLASS_NAME_REGEX = 'class\\s*(\\w+(<\\w*>)?)\\s*[(]?'
+    CLASS_NAME_REGEX = '((\\w+)\\s+)?class\\s*(\\w+(<[\\w\\s]*>)?)\\s*'
     INTERFACE_NAME_REGEX = 'interface\\s*([\\w<>]+)\\s*[^(]'
     # print(file_path)
     flag = None
     with open(file_path, 'r') as file:
         for line in file:
-            if "interface " in line:
-                flag = KotlinInterface(re.search(INTERFACE_NAME_REGEX, line).group(1))
-                interface_list.append(flag)
-            elif "class " in line:
-                flag = KotlinClass(re.search(CLASS_NAME_REGEX, line).group(1))
-                class_list.append(flag)
-            if flag is not None:
-                flag.collect(line)
-                if not flag.shouldContinue:
-                    break
+            if flag is None:
+                search_result = re.search(INTERFACE_NAME_REGEX, line)
+                if search_result is not None:
+                    flag = KotlinInterface(search_result.group(1))
+                    interface_list.append(flag)
+                search_result = re.search(CLASS_NAME_REGEX, line)
+                if "class " in line:
+                    class_type = search_result.group(2)
+                    if class_type is not None:
+                        if class_type == "data" or class_type == "sealed" or class_type == "enum":
+                            continue
+                    else:
+                        class_type = ""
+                    flag = KotlinClass(search_result.group(3), class_type)
+                    class_list.append(flag)
+                else:
+                    continue
+            flag.collect(line)
+            if not flag.shouldContinue:
+                break
+
+
+def write_data_on_file():
+    with open(OUTPUT_FILE_PATH, "w") as file:
+        for kt_class in class_list:
+            file.write(kt_class.get_signature() + "\n")
+            for class_property in kt_class.overrides:
+                file.write(class_property + "\n")
+            file.write("}\n\n\n")
+        for kt_class in class_list:
+            for dependency in kt_class.inputs:
+                file.write(kt_class.name + " --> " + dependency + "\n")
+
+        file.close()
 
 
 directory_path = "/Users/user/Documents/Android Studio Project/driver-app/feature/loan/src"
 print_files(directory_path)
+
 for item in interface_list:
     print(item)
-print('-'*50)
+print('-' * 50)
 for item in class_list:
     print(item)
 
+write_data_on_file()
